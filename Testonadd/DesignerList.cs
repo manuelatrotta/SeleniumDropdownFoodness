c096beef24d1d097a22c96226b28ef6eb6b95ebf
Manuela Trotta <MTrotta@qubicaamf.com>
2021-07-28 21:41:49 +0200

    Add project files.

---
+++ Testonadd/DesignerList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testonadd
+{
+    public class DesignerList : ArrayList
+    {
+        // Create an empty ArrayList, and add some elements.
+       
+        ArrayList Designerlist = new ArrayList();
+
+        
+    }
+
+   
+}
+
---
+++ Testonadd/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections;
+using System.Threading;
+
+
+namespace Testonadd
+{
+
+
+    class Program
+    {
+
+        static void Main()
+        {
+            String baseURL = "https://shop.foodness.it/prodotti/bio/bevanda-dorzo-bio/";
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(baseURL);
+            // IWebElement drpProdotti = driver.FindElement(By.CssSelector("#pa_compatibilita>option:nth-child(2)"));
+
+            Thread.Sleep(20000);
+            //add a option present in a select element 
+            for (int i=1; i< 5; i++)
+            {
+                IWebElement drpProdotti = driver.FindElement(By.CssSelector("#pa_compatibilita>option:nth-child(" + i + ")"));
+                if (drpProdotti.Displayed)
+                {
+                    Console.WriteLine("L'elemento nella selezione in posizione: " + i + " con valore: " + drpProdotti.GetAttribute("value"));
+                }
+            }
+            
+
+
+
+
+
+        }
+
+    }
+}
+
